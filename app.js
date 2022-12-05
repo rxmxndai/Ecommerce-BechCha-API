99d2b667ed1f090c2f63dc82b157b80c2203de28
RXMXNDAI <k.romann413@gmail.com>
2022-12-05 21:19:06 +0545

    Database setup MONGODB

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+const userRoute = require('./routes/user')
 
 const app = express();
-// mongoose.connect(URL)
+
+dotenv.config();
+
+mongooseKey = process.env.MONGOOSE_SECRET_KEY;
+
+mongoose.connect(mongooseKey)
+.then(console.log("Database connected Successfully !"))
+.catch( err => console.log(err))
 
 
 
+app.use("/api/user", userRoute);
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server live at PORT :"+ PORT);
+    console.log("Server live at PORT :" + PORT);
 })
---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const router = require("express").Router()
+
+
+router.get("/usertest", (req, res) =>  {
+    res.send("User test")
+});
+
+
+module.exports = router
