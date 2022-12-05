d59188469c680949e3729e58f0d4c1da29d58484
RXMXNDAI <k.romann413@gmail.com>
2022-12-05 23:51:12 +0545

    Deep Email Vlidator for signup

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,13 @@ const router = require("express").Router()
 var CryptoJS = require("crypto-js");
 const User = require("../models/User")
 
+const emailValidator = require('deep-email-validator');
+const jwt = require('jsonwebtoken')
+
+async function isEmailValid(email) {
+    return emailValidator.validate(email)
+}
+
 // register user
 router.post("/register", async (req, res) =>  {
     const newUser = new User({
@@ -9,6 +16,16 @@ router.post("/register", async (req, res) =>  {
         email: req.body.email,
         password: CryptoJS.AES.encrypt(req.body.password, process.env.CRYPTO_SALT).toString(),
     });
+
+    const {valid, validators, reason} = await isEmailValid(req.body.email)
+
+    if (!valid)  {
+        res.status(400).json({
+            message: "Invalid Email detected !",
+            reason: validators[reason].reason
+        });
+    }
+
     // save to database
     try {
         const savedUser = await newUser.save();
@@ -32,12 +49,19 @@ router.post("/login", async (req, res) => {
         const pass = CryptoJS.AES.decrypt(user.password, process.env.CRYPTO_SALT);
         const actualPassword = pass.toString(CryptoJS.enc.Utf8);
 
-
         if (actualPassword !== req.body.password) res.status(401).json("No such user registered.")
         
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin,
+            }, 
+            process.env.JWT_SECRET_KEY,
+            {expiresIn: "1d"}    
+        );
+
         const { password, ...rest } = user._doc;
 
-        res.status(200).send(rest);
+        res.status(200).send( {...rest, accessToken} );
 
     }
     catch (err) {
