7933574a3c2f2a5cdee02d6ea99ab421e32f6cfa
rxmxndai <k.romann413@gmail.com>
2023-01-17 13:21:47 +0545

    sharp for image process instead of shortid

--- routes/products.js
+++ routes/products.js
@@ -1,25 +1,8 @@
-const Product = require("../models/Product");
-const Category  = require("../models/Category");
 const router = require("express").Router();
-const path = require("path")
-
 const multer = require("multer")
-const shortid = require("shortid")
 const { verifyTokenAndAdmin }  = require("../middlewares/auth");
-const { JOIproductSchemaValidate } = require("../middlewares/JoiValidator");
 const { addProduct, updateProduct, deleteProduct, getOneProduct, getAllProducts } = require("../controller/Product/product");
 const customError = require("../utils/customError");
-
-
-
-// const storage = multer.diskStorage({
-//     destination: function(req, file, callback) {
-//         callback(null, path.join(path.dirname(__dirname), "products"));
-//     },
-//     filename: function (req, file, callback) {
-//         callback(null, shortid.generate() + "-" + file.originalname)
-//     }
-// })
 
 
 const upload = multer({
